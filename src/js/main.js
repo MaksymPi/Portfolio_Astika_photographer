bab7807bbf46e19584e25484b95fbf7582298d6a
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-03-07 20:51:44 +0100

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import '../scss/style.scss'
+
+const links = document.querySelectorAll('.menu__link');
+
+for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function(event){
+        event.preventDefault(); 
+        let current = document.querySelector(".menu__link.active");
+        if (current) {
+            current.classList.remove("active");
+        }
+        this.classList.add("active");
+    })
+}
