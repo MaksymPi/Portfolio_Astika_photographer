487a7328ffaa89285fb25b0ae0587097db870661
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-03-11 20:48:37 +0100

    section

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,20 @@
 import '../scss/style.scss'
 
+
 const links = document.querySelectorAll('.menu__link');
 
-for(let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function(event){
-        event.preventDefault(); 
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function (event) {
+        event.preventDefault();
         let current = document.querySelector(".menu__link.active");
         if (current) {
             current.classList.remove("active");
         }
         this.classList.add("active");
+        
+        // Smooth scrolling to the target section
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: "smooth" });
     })
 }
