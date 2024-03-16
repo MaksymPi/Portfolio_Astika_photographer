0de15241308764713962a7ab1d094d4a5f228497
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-03-16 14:59:25 +0100

    dist

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import '../scss/style.scss'
 
-
 const links = document.querySelectorAll('.menu__link');
 
 for (let i = 0; i < links.length; i++) {
@@ -18,4 +17,82 @@ for (let i = 0; i < links.length; i++) {
         targetSection.scrollIntoView({ behavior: "smooth" });
     })
 }
+
+// ---------------------------------Modal-Family------------------------------------------
+
+document.getElementById("family-open-modal-btn").addEventListener("click", function() {
+    document.getElementById("family-modal").classList.add("open");
+});
+
+
+// ---------------------------------Close----------------------------------------------
+
+document.getElementById("family-close-modal-btn").addEventListener("click", function() {
+    document.getElementById("family-modal").classList.remove("open");
+});
+
+// -------------------------Close Esc----------------------------------------------
+
+window.addEventListener('keydown', (e) => {
+    if (e.key ==='Escape') {
+        document.getElementById("family-modal").classList.remove("open");
+    }
+})
+
+
+// ---------------------------------Modal-Individual------------------------------------------
+
+document.getElementById("individual-open-modal-btn").addEventListener("click", function() {
+    document.getElementById("individual-modal").classList.add("open");
+});
+
+
+// ---------------------------------Close----------------------------------------------
+
+document.getElementById("individual-close-modal-btn").addEventListener("click", function() {
+    document.getElementById("individual-modal").classList.remove("open");
+});
+
+// ---------------------------------Close Esc----------------------------------------------
+
+window.addEventListener('keydown', (e) => {
+    if (e.key ==='Escape') {
+        document.getElementById("individual-modal").classList.remove("open");
+    }
+})
+
+// ---------------------------------Modal-love------------------------------------------
+
+document.getElementById("love-open-modal-btn").addEventListener("click", function() {
+    document.getElementById("love-modal").classList.add("open");
+});
+
+
+// ---------------------------------Close----------------------------------------------
+
+document.getElementById("love-close-modal-btn").addEventListener("click", function() {
+    document.getElementById("love-modal").classList.remove("open");
+});
+
+// ------------------Close Esc------------------------------------
+
+window.addEventListener('keydown', (e) => {
+    if (e.key ==='Escape') {
+        document.getElementById("love-modal").classList.remove("open");
+    }
+})
+
+// -------------------Open img----------------------------
+
+document.querySelectorAll('.img-container img').forEach(img =>{
+    img.onclick = () => {
+        document.querySelector('.pop-up').style.display = 'block';
+        document.querySelector('.pop-up img').src = img.getAttribute('src')
+    }
+});
+
+document.querySelector('.pop-up__close-btn').onclick = () => {
+    document.querySelector('.pop-up').style.display = 'none';
+}
+
 
